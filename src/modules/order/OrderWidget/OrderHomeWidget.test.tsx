7516bc12bfedf0dafd6e21cd89eeb698f6480334
NTNET\hussaimu <sd>
2023-01-13 12:02:31 +0530

    no message

--- src/modules/order/OrderWidget/OrderHomeWidget.test.tsx
+++ src/modules/order/OrderWidget/OrderHomeWidget.test.tsx
@@ -3,6 +3,11 @@ import OrderHomeWidget from './OrderHomeWidget'
 import reduxConnectedCompRender from '@_test/reduxConnectedCompRender'
 import { act } from 'react-dom/test-utils'
 
+import MockAdapter from 'axios-mock-adapter'
+import axios from 'axios'
+
+var mockAxios = new MockAdapter(axios);
+
 // beforeAll(() => {
 //   init()
 // });
@@ -11,13 +16,15 @@ import { act } from 'react-dom/test-utils'
 
 /*eslint-disable */
 beforeEach(() => {
-  console.log('-----beforeEach: file level------')
-  const _fetch: any = fetch
-  _fetch.resetMocks()
+  // console.log('-----beforeEach: file level------')
+  // const _fetch: any = fetch
+  // fetch.resetMocks()
+  mockAxios.reset();
 })
 afterEach(() => {
-  console.log('-----afterEach: file level------')
+  // console.log('-----afterEach: file level------')
   // cleanup()
+  mockAxios.reset();
 })
 
 describe('Testing Order Home widget Success', () => {
@@ -30,21 +37,24 @@ describe('Testing Order Home widget Success', () => {
   }
 
   beforeAll(() => {
-    console.log('--------beforeAll: in describe 1---------')
+    // console.log('--------beforeAll: in describe 1---------')
 
-    const _fetch: any = fetch
-    _fetch.mockResponse(JSON.stringify({ totalOrder: 100, pendingOrder: 75, canceledOrder: 25 }))
+    // const _fetch: any = fetch
+    // _fetch.mockResponse(JSON.stringify({ totalOrder: 100, pendingOrder: 75, canceledOrder: 25 }))
+
+    mockAxios.onGet('/api/homeWidget').reply(200, { totalOrder: 100, pendingOrder: 75, canceledOrder: 25 })
+
     reduxConnectedCompRender(<><OrderHomeWidget /></>, orderHomeWidgetReduxState, {})
   })
 
   it('Shoud display Page Title', async () => {
-    console.log('--------it: in first 1---------')
+    // console.log('--------it: in first 1---------')
 
     const sectiontitle = await screen.findByTestId(/sectiontitle/i)
     expect(sectiontitle).toBeInTheDocument()
   })
   it('Shoud display all values of API', async () => {
-    console.log('--------it: in second---------')
+    // console.log('--------it: in second---------')
 
     const items_list_wrap = await screen.findByTestId(/order_items_list_wrap/i)
     expect(items_list_wrap).toBeInTheDocument()
@@ -60,9 +70,11 @@ describe('Testing Order Home widget Success', () => {
   })
 
   it('Shoud updare UI on click refresh', async () => {
-    console.log('--------it: in third---------')
-    const _fetch: any = fetch
-    _fetch.mockResponse(JSON.stringify({ totalOrder: 507567, pendingOrder: 35, canceledOrder: 15 }))
+    // console.log('--------it: in third---------')
+    // const _fetch: any = fetch
+    // _fetch.mockResponse(JSON.stringify({ totalOrder: 507567, pendingOrder: 35, canceledOrder: 15 }))
+
+    mockAxios.onGet('/api/homeWidget').reply(200, { totalOrder: 507567, pendingOrder: 35, canceledOrder: 15 })
 
     fireEvent.click(screen.getByTestId('refresh'))
     const newTotalVal = await screen.findByText('507567')
@@ -70,7 +82,7 @@ describe('Testing Order Home widget Success', () => {
   })
 
   afterAll(() => {
-    console.log('--------afterAll: in describe 1---------')
+    // console.log('--------afterAll: in describe 1---------')
     cleanup()
   })
 })
@@ -81,10 +93,12 @@ describe('Testing Order Home widget API Fail', () => {
     }
   }
   beforeAll(() => {
-    console.log('--------beforeAll: in describe 2---------')
+    // console.log('--------beforeAll: in describe 2---------')
 
-    const _fetch: any = fetch
-    _fetch.mockReject('fake error message')
+    // const _fetch: any = fetch
+    // _fetch.mockReject('fake error message')
+    mockAxios.onGet('/api/homeWidget').reply(500)
+
     reduxConnectedCompRender(<><OrderHomeWidget /></>, orderHomeWidgetReduxState, {})
   })
 
@@ -94,11 +108,11 @@ describe('Testing Order Home widget API Fail', () => {
 
     await waitFor(() => expect(screen.getByTestId(/error/i)).toBeInTheDocument())
 
-    expect(screen.getByText('fake error message')).toBeInTheDocument()
+    expect(screen.getByText('Something went wrong')).toBeInTheDocument()
   })
 
   afterAll(() => {
-    console.log('--------afterAll: in describe 2---------')
+    // console.log('--------afterAll: in describe 2---------')
     cleanup()
   })
 })
